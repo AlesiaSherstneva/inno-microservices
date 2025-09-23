3ad26ab12570f1802ef21c6bac0b954723f6fee6
Alesia Sherstneva <alesiasherstneva@gmail.com>
2025-09-23 10:04:17 +0300

    USR-3: create entities

---
+++ user-service/src/main/java/com/innowise/userservice/entity/CardInfo.java
@@ -0,0 +1,43 @@
+package com.innowise.userservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "card_info")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CardInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "number")
+    private String number;
+
+    @Column(name = "holder")
+    private String holder;
+
+    @Column(name = "expiration_date")
+    private LocalDate expirationDate;
+}
