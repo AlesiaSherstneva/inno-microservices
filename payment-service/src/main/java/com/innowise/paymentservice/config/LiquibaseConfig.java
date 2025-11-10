f3cc353742d8b783cfb3b06ff754444773fb0dca
Alesia Sherstneva <alesiasherstneva@gmail.com>
2025-11-10 18:56:59 +0300

    PMNT-2: add containerization for PaymentService

--- payment-service/src/main/java/com/innowise/paymentservice/config/LiquibaseConfig.java
+++ payment-service/src/main/java/com/innowise/paymentservice/config/LiquibaseConfig.java
@@ -4,6 +4,7 @@ import liquibase.Liquibase;
 import liquibase.database.DatabaseFactory;
 import liquibase.ext.mongodb.database.MongoLiquibaseDatabase;
 import liquibase.resource.ClassLoaderResourceAccessor;
+import liquibase.resource.ResourceAccessor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -26,10 +27,12 @@ public class LiquibaseConfig {
     @Bean
     @DependsOn("mongoTemplate")
     public Liquibase liquibase() throws Exception {
-        MongoLiquibaseDatabase database = (MongoLiquibaseDatabase) DatabaseFactory.getInstance()
-                .openDatabase(mongoDbUri, username, password, null, null);
+        ResourceAccessor resourceAccessor = new ClassLoaderResourceAccessor();
 
-        Liquibase liquibase = new Liquibase(changeLog, new ClassLoaderResourceAccessor(), database);
+        MongoLiquibaseDatabase database = (MongoLiquibaseDatabase) DatabaseFactory.getInstance()
+                .openDatabase(mongoDbUri, username, password, null, resourceAccessor);
+
+        Liquibase liquibase = new Liquibase(changeLog, resourceAccessor, database);
         liquibase.update("");
 
         return liquibase;
