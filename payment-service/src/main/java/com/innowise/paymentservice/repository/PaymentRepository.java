4d4d7cfcab90259324b0319d911a00c7f871c5e8
Alesia Sherstneva <alesiasherstneva@gmail.com>
2025-11-14 14:16:19 +0300

    PMNT-6: write Kafka integration tests in the payment-service app

--- payment-service/src/main/java/com/innowise/paymentservice/repository/PaymentRepository.java
+++ payment-service/src/main/java/com/innowise/paymentservice/repository/PaymentRepository.java
@@ -4,6 +4,9 @@ import com.innowise.paymentservice.model.entity.Payment;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PaymentRepository extends MongoRepository<Payment, String> {
+    Optional<Payment> findPaymentByOrderId(Long orderId);
 }
